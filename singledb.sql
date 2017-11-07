6a5dc70482c2cab3ce0ed3dfa865f429e719ba18
Robins <tharakan@gmail.com>
2017-11-07 22:30:52 +1100

    Create SingleDB SQL that when run on a DB would try to pre-fetch all DB Objects owned by that User
    Create SingleDB SQL that when run on a DB would try to pre-fetch all DB Objects owned by that User.
    
     * This Script allows an an RDS User to pre-fetch most of the Database
     * objects (owned by the user that triggers the SQL) to Cache, which 
     * effectively reduces the effect of Lazy Loading seen on AWS EBS.
     *
     * It doesn't pre-fetch the following:
     * 1. Toast tables [RDS limitation] - No workaround
     * 2. Indexes [pg_prewarm limitation] - No workaround
     * 3. DB Objects owned by other users [RDS limitation]
     * 3a. Workaround: Re-run SQL as other User

---
+++ singledb.sql
@@ -0,0 +1,27 @@
+/*
+ * This Script allows an an RDS User to pre-fetch most of the Database
+ * objects (owned by the user that triggers the SQL) to Cache, which 
+ * effectively reduces the effect of Lazy Loading seen on AWS EBS.
+ *
+ * It doesn't pre-fetch the following:
+ * 1. Toast tables [RDS limitation] - No workaround
+ * 2. Indexes [pg_prewarm limitation] - No workaround
+ * 3. DB Objects owned by other users [RDS limitation]
+ * 3a. Workaround: Re-run SQL as other User
+ */
+
+CREATE EXTENSION IF NOT EXISTS dblink;
+CREATE EXTENSION IF NOT EXISTS pg_prewarm;
+
+      SELECT clock_timestamp(), pg_prewarm(c.oid::regclass), 
+      relkind, current_database(), n.nspname, c.relname
+      FROM pg_class c
+        JOIN pg_namespace n
+          ON n.oid = c.relnamespace
+        JOIN pg_user u
+          ON u.usesysid = c.relowner
+      WHERE u.usename NOT IN ('rdsadmin', 'rdsrepladmin', ' pg_signal_backend', 'rds_superuser', 'rds_replication')
+      ORDER BY c.relpages DESC;
+   
+DROP EXTENSION IF EXISTS pg_prewarm;
+DROP EXTENSION IF EXISTS dblink;
