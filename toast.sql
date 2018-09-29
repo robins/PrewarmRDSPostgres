fef800b574fec86dbd1b5638cd838ab8a00e3a69
Robins <tharakan@gmail.com>
2018-09-29 16:42:31 +1000

    Simplify SQL for TOAST. Can't find a simple workaround to getting permissions for pg_toast objects. VACUUMing the said tables is much simpler

---
+++ toast.sql
@@ -0,0 +1,14 @@
+/*
+ * Brief: This Script is an extension of the singledb.sql and focusses on 
+ * prefetch of only TOAST data.
+ *
+ * Importantly, do note that owing to how pg_toast schema permission works,
+ * it'd be simplest to run the SQL that is generated as output of the SQL
+ * given below.
+ */
+ 
+SELECT 
+  'VACUUM FULL ' || relname || ';'
+FROM pg_class 
+WHERE reltoastrelid > 0
+ORDER BY 1;
